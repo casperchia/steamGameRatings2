2da61ced6635944daca60a7df3d94ab02a9a279c
Casper Chia <casperchiakyechuen@gmail.com>
2015-07-07 02:19:42 +1000

    Give different names for results urls

--- SGRapp/urls.py
+++ SGRapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^username/(?P<steamName>\w+)/$', views.results, name='results'),
-    url(r'^id/(?P<steamId>[0-9]+)/$', views.results, name='results'),
+    url(r'^username/(?P<steamName>\w+)/$', views.results, name='usernameResults'),
+    url(r'^id/(?P<steamId>[0-9]+)/$', views.results, name='idResults'),
     url(r'^goto/$', views.goto, name='goto'),
     # url(r'^username/(?P<username>\w+)/$', views.results, name='results'),
 ]
