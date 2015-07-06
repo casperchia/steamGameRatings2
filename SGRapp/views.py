a6f5ae23c0f5396c7fd3a4f15dd7f807b018dd9b
Casper Chia <casperchiakyechuen@gmail.com>
2015-07-07 01:27:29 +1000

    Display basic user info

--- SGRapp/views.py
+++ SGRapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from .models import Game
 import steamapi
+from steamGameRatings.settings import STEAM_API_KEY
 
 
 def index(request):
@@ -14,14 +15,19 @@ def index(request):
 
 
 def results(request, steamId=None, steamName=None):
+    steamapi.core.APIConnection(api_key=STEAM_API_KEY)
     if steamId is not None:
-        context = {
-            'username': steamId
-        }
+        user = steamapi.user.SteamUser(steamId)
+
     if steamName is not None:
-        context = {
-            'username': steamName
-        }
+        user = steamapi.user.SteamUser(userurl=steamName)
+
+    context = {
+        'steamName': user.name,
+        'steamId': user.id,
+        'games': user.games,
+        'avatarImg': user.avatar_medium,
+    }
     return render(request, 'SGRapp/results.html', context)
 
 
