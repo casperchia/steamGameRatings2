89c7aba4055b44ad493d125f4e9dd1694581cc77
Casper Chia <casperchiakyechuen@gmail.com>
2015-07-07 00:45:55 +1000

    Add steamapi and SteamAPIKey in settings template

--- SGRapp/views.py
+++ SGRapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from .models import Game
+import steamapi
 
 
 def index(request):
