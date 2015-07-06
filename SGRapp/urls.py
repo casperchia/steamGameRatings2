4d8af418623d28d363141488ef1bdf8b2f136473
Casper Chia <casperchiakyechuen@gmail.com>
2015-07-07 00:20:53 +1000

    Add views for taking in steamName / steamId

--- SGRapp/urls.py
+++ SGRapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^username/(?P<steamName>\w+)/$', views.results, name='results'),
+    url(r'^id/(?P<steamId>[0-9]+)/$', views.results, name='results'),
+    url(r'^goto/$', views.goto, name='goto'),
+    # url(r'^username/(?P<username>\w+)/$', views.results, name='results'),
 ]
--- SGRapp/views.py
+++ SGRapp/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 from .models import Game
 
 
 def index(request):
-    games_list = Game.objects.all()
+    # games_list = Game.objects.all()
     context = {
-        'games_list': games_list
+        # 'games_list': games_list
     }
     return render(request, 'SGRapp/index.html', context)
+
+
+def results(request, steamId=None, steamName=None):
+    if steamId is not None:
+        context = {
+            'username': steamId
+        }
+    if steamName is not None:
+        context = {
+            'username': steamName
+        }
+    return render(request, 'SGRapp/results.html', context)
+
+
+def goto(request):
+    userInput = request.GET.get('userInput')
+    if request.GET.get('inputType') == 'Steam Name':
+        return HttpResponseRedirect('/username/' + userInput)
+    else:
+        return HttpResponseRedirect('/id/' + userInput)
