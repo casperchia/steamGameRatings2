f249450ec75ac5cf06b68c3b5b3eeea344b61e0f
Casper Chia <casperchiakyechuen@gmail.com>
2015-07-11 03:17:25 +1000

    Set DJANGO_SETTINGS_MODULE in os.environ

--- scrape.py
+++ scrape.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+import os
 import django
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'steamGameRatings.settings'
+
 import scrapy
 from SGRapp.models import Game
 
@@ -7,4 +11,7 @@ from SGRapp.models import Game
 def main():
     print 'Starting steam games web scrape...\n'
 
-    print '\nDone.\n'
+    print '\nDone.'
+
+if __name__ == '__main__':
+    main()
